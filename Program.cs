be2462d363a40c025f8653ab4d6d8f03f6ce65c1
Jill Kuchman <pillanoid@gmail.com>
2016-04-06 11:07:32 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace TwilioTestConsole01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name?");
+            var name = Console.ReadLine();
+            var client = new RestClient("https://api.twilio.com/2010-04-01");
+            var request = new RestRequest("Accounts/" + EnvironmentVariables.TwilioAccountSid + "/Messages.json", Method.POST);
+            request.AddParameter("To", "+14049812892");
+            request.AddParameter("From", "+16786078332");
+            request.AddParameter("Body", "Hey there, " + name + "!");
+            client.BaseUrl = new Uri("https://api.twilio.com/2010-04-01");
+            client.Authenticator = new HttpBasicAuthenticator(EnvironmentVariables.TwilioAccountSid, EnvironmentVariables.TwilioAuthToken);
+            var response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            Console.ReadLine();
+        }
+    }
+}
